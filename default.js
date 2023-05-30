9e91723e8e82d63a4d45e39cf9a293ff898de574
Jihyeon Kim <kwlgus3579@naver.com>
2023-05-30 13:23:30 +0900

    문자열 연결하기

--- default.js
+++ default.js
@@ -1,30 +1,65 @@
+var lastName = "홍";
+var firstName = "길동";
+
+var fullName = lastName + firstName;
+
+console.log(fullName);
+console.log("Today is " + "a "+"beautiful day");
+console.log("My name is "+ fullName);
+
+var num1 = 20;
+var num2 = 30;
+var sum = num1 + num2;
+console.log(num1 + num2);
+console.log("" + num1 + num2); //num1, num2 문자열 취급
+console.log(num1 + " + " + num2 + " = " + sum);
+
+
+
+//var name = "이승훈";
+//var age = 29;
+//var cgpa = 3.92;
+//var lineBreak = "<br/>";
+//
+//document.write("이름 : " + name + lineBreak);
+//document.write("나이 : " + age + lineBreak);
+//document.write("학점 : " + cgpa + lineBreak);
+
+
+
+
+
+
+
 //console.log(typeof 123);
-//console.log(typeof 123.5);
-//console.log(typeof "123");
-//console.log(true, typeof true);
-//console.log(false, typeof false);
-
-var car;
-console.log(typeof car);
-var car="";
-console.log(typeof car);
-
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-console.log(typeof person, pseson);
-person=null;
-console.log(typeof person, person);
-
-
-/*document.write('Hello World 2');
-document.write("<h1>Welcome to JS Program<h1/>");
-document.write("<h2>Welcome to JS Program<h2/>");
-
-console.log("Welcome JS Program");
-console.info("Welcome JS Program");
-console.warn("Welcome JS Program");
-console.error("Welcome JS Program");
-
-alert('Welcome JS Program');
-var a = prompt('Welcome JS Program');
-console.log(a);
-*/
+////console.log(typeof 123.5);
+////console.log(typeof "123");
+////console.log(true, typeof true);
+////console.log(false, typeof false);
+//
+//var car;
+//console.log(typeof car);
+//var car="";
+//console.log(typeof car);
+//
+//var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+//console.log(typeof person, person);
+//person=null;
+//console.log(typeof person, person);
+//
+//
+//
+//
+///*document.write('Hello World 2');
+//document.write("<h1>Welcome to JS Program<h1/>");
+//document.write("<h2>Welcome to JS Program<h2/>");
+//
+//console.log("Welcome JS Program");
+//console.info("Welcome JS Program");
+//console.warn("Welcome JS Program");
+//console.error("Welcome JS Program");
+//
+//alert('Welcome JS Program');
+//var a = prompt('Welcome JS Program');
+//console.log(a);
+//*/
