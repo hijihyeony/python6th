eadb4b97609a63b18c471f733fe1cb48b064cc36
Jihyeon Kim <kwlgus3579@naver.com>
2023-05-30 11:50:54 +0900

    javascript 실습

--- default.js
+++ default.js
@@ -1,1 +1,30 @@
-document.write('Hello World 2');
+//console.log(typeof 123);
+//console.log(typeof 123.5);
+//console.log(typeof "123");
+//console.log(true, typeof true);
+//console.log(false, typeof false);
+
+var car;
+console.log(typeof car);
+var car="";
+console.log(typeof car);
+
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(typeof person, pseson);
+person=null;
+console.log(typeof person, person);
+
+
+/*document.write('Hello World 2');
+document.write("<h1>Welcome to JS Program<h1/>");
+document.write("<h2>Welcome to JS Program<h2/>");
+
+console.log("Welcome JS Program");
+console.info("Welcome JS Program");
+console.warn("Welcome JS Program");
+console.error("Welcome JS Program");
+
+alert('Welcome JS Program');
+var a = prompt('Welcome JS Program');
+console.log(a);
+*/
