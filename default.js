8529f2a89f008e6ba61f1e1cd46731c62d092a65
Jihyeon Kim <kwlgus3579@naver.com>
2023-05-31 11:01:14 +0900

    조건문 제어문

--- default.js
+++ default.js
@@ -1,32 +1,67 @@
 
-var num1 = 20;
-var num2 = 10;
-var num3 = "10";
-var num4 = 20;
-var num5 = 15;
+var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
+var num2 = parseInt(prompt('두번째 숫자 입력 : '));
 
-console.log('일반 크기 비교');
-console.log(num1 > num2, num1, '>', num2);
-console.log(num1 >= num2, num1, '>=', num2);
-console.log(num1 < num2, num1, '<', num2);
-console.log(num1 <= num2, num1, '<=', num2);
 
-console.log('같은지 여부 확인');
-console.log(num1 == num4, num1, '==', num4); // == : 비교 연산자 같다 (값 비교)
-console.log(num1 != num4, num1, '!=', num4); // != : 같지 않다 (! : ~이 아니다)
+//if로만 표현 - 매번 실행
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+}
+if(num1 < num2) {
+    console.log("큰 수는 num2 : " + num2);
+}
+if(num1 == num2) {
+    console.log("같은 수");
+}
 
-console.log('===');
-console.log(num1 === num3, num1, '===', num3); // === : 타입이 같다
-console.log(num2 === num3, num2, '===', num3);
-console.log(num2 == num3, num2, '==', num3);
+//else if 사용 - 참일 때 까지만 실행 - 효율적
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+} else if (num1 < num2) {
+    console.log("큰 수는 num2 : " + num2);
+} else if (num1 == num2) {
+    console.log("같은 수");
+}
 
-console.log('논리 연산자');
-console.log('num1 > num2 && num1 < num5', num1 > num2 && num1 < num5);
-// And 연산자 : 둘다 참이어야 참
-console.log('num1 > num2 || num1 < num5', num1 > num2 || num1 < num5);
-// Or 연산자 : 둘 중에 하나만 참이어도 참
-console.log('num1 > num2 && !(num1 < num5)', num1 > num2 && !(num1 < num5));
-// Or 연산자 : 둘 중에 하나만 참이어도 참
+//else 사용 : 예외 조건에 대한 대비
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+} else if (num1 < num2) {
+    console.log("큰 수는 num2 : " + num2);
+} else {
+    console.log("같은 수");
+}
+
+
+
+//var num1 = 20;
+//var num2 = 10;
+//var num3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//
+//console.log('일반 크기 비교');
+//console.log(num1 > num2, num1, '>', num2);
+//console.log(num1 >= num2, num1, '>=', num2);
+//console.log(num1 < num2, num1, '<', num2);
+//console.log(num1 <= num2, num1, '<=', num2);
+//
+//console.log('같은지 여부 확인');
+//console.log(num1 == num4, num1, '==', num4); // == : 비교 연산자 같다 (값 비교)
+//console.log(num1 != num4, num1, '!=', num4); // != : 같지 않다 (! : ~이 아니다)
+//
+//console.log('===');
+//console.log(num1 === num3, num1, '===', num3); // === : 타입이 같다
+//console.log(num2 === num3, num2, '===', num3);
+//console.log(num2 == num3, num2, '==', num3);
+//
+//console.log('논리 연산자');
+//console.log('num1 > num2 && num1 < num5', num1 > num2 && num1 < num5);
+//// And 연산자 : 둘다 참이어야 참
+//console.log('num1 > num2 || num1 < num5', num1 > num2 || num1 < num5);
+//// Or 연산자 : 둘 중에 하나만 참이어도 참
+//console.log('num1 > num2 && !(num1 < num5)', num1 > num2 && !(num1 < num5));
+//// Or 연산자 : 둘 중에 하나만 참이어도 참
 
 
 
